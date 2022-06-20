72725c87d863b4eca9798c13e8cc3b177f5fa8f2
sanditomaz <sandi.contato@gmail.com>
2022-06-20 01:50:22 -0300

    Added last part + a bonus

--- script.js
+++ script.js
@@ -8,7 +8,11 @@ let animatedCards = [
     "unicornparrot"
   ];
 
+let cardsUp;
+let cont;
+let globalNumberOfCards;
   
+
 function comparador() {
     return Math.random() - 0.5;
 }
@@ -45,10 +49,10 @@ function createCards(numberOfCards) {
       document.querySelector("ul").innerHTML += cardsTemplate;
     }
   
-    let sevencards = document.querySelector("ul");
+    let sevenCards = document.querySelector("ul");
 
-    for (let i = sevencards.children.length; i >= 0; i--) {
-      sevencards.appendChild(sevencards.children[(Math.random() * i) | 0]);
+    for (let i = sevenCards.children.length; i >= 0; i--) {
+      sevenCards.appendChild(sevenCards.children[(Math.random() * i) | 0]);
     }//This loop above shuffles ALL of the cards
 }
 
@@ -57,47 +61,73 @@ askHowManyCardsWantsToPlay();
 createCards();
 
 
-
 function askHowManyCardsWantsToPlay() {
-    //let howManyCards = Number(
-    //   prompt(
-    //     "How many cards do you want to play with?\n Pick even numbers from 4 to 14 only."
-    //   )
-    // );
+    let howManyCards = Number(
+      prompt(
+        "Welcome To The Parrot Game !! 😃😃\n\nHow many cards do you want to play with ?\nRemember, you can only pick even numbers from 4 to 14.\n\n Enjoy ! ⚡⚡"
+      )
+    );
   
-    // while (howManyCards < 4 || howManyCards > 14 || howManyCards % 2 !== 0) {
-    //   howManyCards = Number(
-    //     prompt(
-    //       "How many cards do you want to play with?\n Pick even numbers from 4 to 14 only."
-    //     )
-    //   );
-    // }
-  
-    //let numberOfCards = howManyCards / 2;
-    let numberOfCards = 2;
+    while (howManyCards < 4 || howManyCards > 14 || howManyCards % 2 !== 0) {
+      howManyCards = Number(
+        prompt(
+          "Welcome To The Parrot Game !! 😃😃\n\nHow many cards do you want to play with ?\nRemember, you can only pick even numbers from 4 to 14.\n\n Enjoy ! ⚡⚡"
+        )
+      );
+    }
+    globalNumberOfCards = howManyCards;
+    let numberOfCards = howManyCards / 2;
+    
     createCards(numberOfCards);
 }
 
 
-
 function play(clickedCard){
     clickedCard.classList.add("turn"); //
-    const cardsUp= document.querySelectorAll(".turn"); //
-        
+    cardsUp= document.querySelectorAll(".turn"); //    
+
     if(cardsUp.length === 2){ 
         compareCards(cardsUp); //
     }
+    endGame();
 }
 
-
+let numberOfRounds = 2;
 
 function compareCards(cards){ //
+    if(cards){
+        numberOfRounds = numberOfRounds + 2;
+    }
+
     const firstCardImage = cards[0].querySelector(".back-face img"); //
     const secondCardImage =  cards[1].querySelector(".back-face img");
     
     if(firstCardImage.src === secondCardImage.src){ //Compare Srcs
         return;
     }
-    cards[0].classList.remove("turn"); //
+
+    cards[0].classList.remove("turn"); 
     cards[1].classList.remove("turn");
+    setTimeout(compareCards, 1000, );
 }
+
+
+ function endGame(){
+   
+    if(cardsUp.length  ===  globalNumberOfCards ){
+        alert(`Congratulations!! 🎉🎉🎉\n\nYou won the game in ${numberOfRounds} rounds! 👏👏`);
+        restart();
+    }
+    
+}
+
+function restart(){
+    cont = prompt("Would you like to play again? 🕹️🕹️\n\nType 'sim' for a yes or 'não' for a no, then watch the magic happen. 🤞\n");
+
+    if(cont === "sim"){
+        location.reload();
+    }else{
+        alert(`Congratulations!! 🎉🎉🎉\n\nYou won the game in ${numberOfRounds} rounds! 👏👏`);
+    }
+}
+
